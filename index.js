3720232da304b7b029f3592a7e8982d83966af51
Evgeny Mokeev <mokeev@yandex-team.ru>
2016-04-16 14:57:30 +0500

    Cluster

--- index.js
+++ index.js
@@ -3,7 +3,21 @@ if (process.env.DEMO || process.env.RECORD) {
     require('./mock-requests');
 }
 
-const app = require('./app.js');
+const cluster = require('cluster');
+const os = require('os');
 
-app.listen(app.get('port'),
-    () => console.log(`Listening on port ${app.get('port')}`));
+if (cluster.isMaster) {
+    const cpus = os.cpus().length;
+    for (var i = 0; i < cpus; i++) {
+        cluster.fork();
+    }
+} else {
+    const app = require('./app.js');
+
+    // setTimeout(function () {
+    //    throw new Error('Ooops');
+    // }, Math.ceil(Math.random() * 3) * 1000);
+
+    app.listen(app.get('port'),
+        () => console.log(`Listening on port ${app.get('port')}`));
+}
